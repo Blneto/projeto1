97af9871afec19ba74e90f15a71a69fd0e034b57
Luiz <bignamiluiz10@gmail.com>
2023-03-08 16:36:18 -0400

    INITIAL

---
+++ src/components/Oi.svelte
@@ -0,0 +1,23 @@
+<div class="button">
+    <p style="margin: auto">Confirmar</p>    
+</div>
+
+<style>
+    .button {
+        background-color: #FC0;
+        width: 128px;
+        padding: 16px 32px;
+        text-align: center; 
+        display: flex; 
+        margin-top: 24px; 
+        border-radius: 10px;
+        transition: .3s;
+        cursor: pointer;
+        box-shadow: 0px 3px 12px rgba(0,0,0,0.2)
+    }
+
+    .button:hover {
+        transform: translateY(-5px);
+        background-color: #CCC;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+    import Button from "../components/Oi.svelte";
+</script>
+
+<Button></Button>
